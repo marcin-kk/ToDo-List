714ab65f336866b7f6c46eb2baef2a4b4f58b910
Marcin Kula <mkula.poczta@gmail.com>
2023-04-25 13:52:28 +0200

    new functionality added - new task add

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ let toDoInput // place when the user types the task
 let errorInfo // info when no task has been typed in
 let addBtn // add Btn - to add a new task to the list
 let ulList // the task list, ul tags
-
+let newTodo //dynamic created li with a new task
 
 const main = () => {
-    prepareDomElements()
-    prepareDOMEvens()
+	prepareDomElements()
+	prepareDOMEvens()
 }
 
 const prepareDomElements = () => {
-    toDoInput = document.querySelector('.todo-input')
-    errorInfo = document.querySelector('.error-info')
-    addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todolist ul')
+	toDoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
 }
 
 const prepareDOMEvens = () => {
-    //adds event listeners
+	//adds event listeners
+	addBtn.addEventListener('click', addNewTodo)
 }
 
+const addNewTodo = () => {
+	if (toDoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = toDoInput.value
+		ulList.append(newTodo)
+
+		toDoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!'
+	}
+}
 
 document.addEventListener('DOMContentLoaded', main)
